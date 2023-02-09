35b45c283cb1c7c45cfca17b543e9f7e4cb984e7
Pallav2011 <ranpisepg09@gmail.com>
2023-02-08 23:47:06 -0800

    one way data binding is learned
    one way data binding is learned

--- angular project/angularapp/src/app/test2/test2.component.ts
+++ angular project/angularapp/src/app/test2/test2.component.ts
@@ -9,10 +9,23 @@ export class Test2Component implements OnInit {
 age=25;
 adress="Pandharpur";
 pinCode=413304;
+data="";
+count:number=0;
 imageUrl=`https://media-cdn.tripadvisor.com/media/photo-s/15/a4/9b/77/legacy-hotel-at-img-academy.jpg`;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onButtonClick(){
+   this.data="Event is triggered"; 
+  }
+
+  buttonIncreaseValue(){
+    this.count++;
+  }
+  inputData="";
+  valueKeyUp(value){
+    this.inputData=value.target.value;
+  }
 }
